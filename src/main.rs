17bcd733aafa8469d491147cc98610e362407d85
Marcos Soares <marcos.matos@sinch.com>
2022-03-11 12:00:12 -0300

    first commit

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+use actix_web::{get, web, App, HttpServer, Responder};
+
+#[get("/")]
+async fn greet() -> impl Responder {
+    format!("Hello World!")
+}
+
+#[actix_web::main] // or #[tokio::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .route("/hello", web::get().to(|| async { "Hello World!" }))
+            .service(greet)
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
+}
